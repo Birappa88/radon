9fb480594906404c079327bf27a8637798917b29
Birappa88 <birappa.mangod@gmail.com>
2022-06-04 18:31:37 +0530

    its done at 6:30 - 04/06

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,72 +1,107 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
-
 const router = express.Router();
 
-// router.get('/test-me', function (req, res) {
-//     myHelper.printDate()
-//     myHelper.getCurrentMonth()
-//     myHelper.getCohortData()
-//     let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-//     console.log('The first element received from underscope function is '+firstElement)
-//     res.send('My first ever api!')
-// });
-
- router.get('/hello', function (req, res) {
-   
-     res.send('Hello there!')
- });
-
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
-
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
 
 router.get('/movies', function (req, res) {
-   const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-   console.log(movies)
-    res.send('Hello there!')
+    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    res.send(movies)
 });
+
 
 router.get('/movies/1', function (req, res) {
     const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    res.send(movies)
     console.log(movies[1])
-     res.send('Hello there!')
- });
+});
 
 
- router.get('/movies/6', function (req, res) {
-    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    for (let i=0; i<movies.length;i++) {
-         let index = movies[i] 
-         if(i>movies.length){
-    console.log("use valid index")
-         }
-         console.log(movies[6])
+router.get('/movies/:indexNumber', function (req, res) {
+    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    let i = req.params.indexNumber
+    if (i >= movies.length) {
+        res.send('Error : Use valid Index')
     }
-     res.send('Hello njjn there!')
- });
+    else {
+        res.send(movies[i])
+    }
+});
 
- router.get('/films', function (req, res) {
-    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    console.log(movies[1])
-     res.send('Hello there!')
- });
 
+router.get('/films', function (req, res) {
+    const movies = [{
+        "id": 1,
+        "name": "The Shining"
+    }, {
+        "id": 2,
+        "name": "Incendies"
+    }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+    }, {
+        "id": 4,
+        "name": "Finding Nemo"
+    }]
+
+    res.send(movies)
+});
+
+
+router.get('/films/:filmId', function (req, res) {
+    const movies = [{
+        "id": 1,
+        "name": "The Shining"
+    }, {
+        "id": 2,
+        "name": "Incendies"
+    }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+    }, {
+        "id": 4,
+        "name": "Finding Nemo"
+    }]
+
+    let i = req.params.filmId - 1
+    if (i >= movies.length || i < 0) {
+        res.send('No movie exists with this Id')
+    }
+    else
+        res.send(movies[i])
+});
+
+
+
+router.get("/sol1", function (req, res) {
+    let arr= [1,2,3,5,6,7]
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+
+
+router.get("/sol2", function (req, res) {
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+    let missingNumber= consecutiveSum - total
+   
+    res.send(  { data: missingNumber  }  );
+  });
 
 module.exports = router;
-// adding this comment for no reason
