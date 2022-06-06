093b901ee69de5fffd5278f65440b3cec5e59f84
Birappa88 <birappa.mangod@gmail.com>
2022-06-07 02:47:09 +0530

    its partially daone

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,26 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+const bookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        unique: true,
+        required: true
     },
-    sales: {type: Number, default: 10}
+    authorName: String,
+    tags: [String],
+    prices: [
+        {indianPrice: String},
+        {europePrice: String}
+    ],
+    year: {
+        type: Number,
+        default: 2021
+    },
+    totalPages: Number,
+    stockAvailable: Boolean
+
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Newbook', bookSchema) 
 
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
