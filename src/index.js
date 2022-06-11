473e14ff8e7c75b537d29843d02ae18765bbfadf
Birappa88 <birappa.mangod@gmail.com>
2022-06-11 10:48:48 +0530

    its complete

--- src/index.js
+++ src/index.js
@@ -8,18 +8,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Birappa:MangoDB@cluster0.m5phg.mongodb.net/Birappa828?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+
+const globalMw = function (req, res, next) {
+    
+    var currentdate = new Date()
+    var datetime = currentdate.getFullYear() + "-" + (currentdate.getMonth() + 1) + "-" + currentdate.getDate() + " "
+        + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds()
+    let ip = req.ip
+    let url = req.originalUrl
+
+    console.log(`{${datetime}, ${ip}, ${url}`)
+    next()
+}
+
+app.use(globalMw);
 
 app.use('/', route);
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,62 +1,42 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
 
+const midl1 = function (req, res, next) {
+    console.log("Hi I am first middleware named Midl1")
+    next()
+}
+
+const midl2 = function (req, res, next) {
+    console.log("Hi I am second middleware named Midl2")
+    next()
+}
+
+const itsWorking = function (req, res) {
+    res.send({ msg: "All middleware are working" })
+}
 
 
-router.post("/createBook", BookController.createBook  )
+const loginCheck = function (req, res, next) {
+    let mailId = true
 
+    if (mailId == true) { console.log("you are eligible to register") 
+    next();}
+    else
+    res.send("Check your mailId")
+}
 
+const register = function (req, res) {
+  res.send({msg:"you are successfully registered"})
+}
 
-
-// router.post("/createUser", UserController.createUser  )
-// router.get("/getUsersData", UserController.getUsersData)
-
-
-// const mid1= function ( req, res, next) {
-//     console.log("Hi I am a middleware named Mid1")
-//     // logic
-//     let loggedIn = false
-
-//     if (loggedIn== true) { 
-//         console.log( "OK LOGGED IS IS TRUE NOW")
-//         next ()
-//     }
-//     else {
-//         res.send ("Please login or register")
-//     }
-// }
-
-// // e.g. restricted and open-to-all API's can be handled like below now:
-// router.get('/homePage', mid1, UserController.feeds)
-// router.get('/profileDetails', mid1, UserController.profileDetails)
-// router.get('/friendList', mid1, UserController.friendList)
-// router.get('/changePassword', mid1, UserController.changePassword)
-
-// router.get('/termsAndConditions',  UserController.termsAndConditions)
-// router.get('/register',  UserController.register)
-
-
-
-
-
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
-
-
-
-// router.get("/basicRoute2", commonMW.mid1, UserController.basicCode2)
-// router.get("/basicRoute3", commonMW.mid2, UserController.basicCode3)
-// router.get("/basicRoute4", commonMW.mid1, commonMW.mid4, UserController.basicCode4)
-
-
+router.get('/middleWareChecking', midl1, midl2, itsWorking)
+router.get('/register', loginCheck, register)
 
 
 module.exports = router;
