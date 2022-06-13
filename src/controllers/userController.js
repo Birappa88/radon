780c04ce0e2676ef56e18351c121f321aaa771fc
Birappa88 <birappa.mangod@gmail.com>
2022-06-14 00:27:52 +0530

    Its Done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,47 +1,62 @@
-const UserModel= require("../models/userModel")
+const UserModel = require("../models/userModel")
+const ProductModel = require("../models/productModel")
+const OrderModel = require("../models/orderModel")
 
 
+const createProduct = async function (req, res) {
+    let ProductCreated = await ProductModel.create(req.body)
+    res.send(ProductCreated)
+}
 
-
-const basicCode= async function(req, res, next) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
-
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    //res.send({ msg: "This is coming from controller (handler)"})
+const middleware = async function (req, res, next) {
+    let FreeAppUser = req.headers.isfreeappuser
+    if (!FreeAppUser) { res.send({ Error: "request is missing a mandatory header" }) }
+    let a = false
+    if (FreeAppUser == "true") {
+        a = true
+    }
+    req.isFreeAppUser = a
     next()
+}
+
+const createUser = async function (req, res) {
+    let UserCreated = await UserModel.create(req.body)
+    res.send({ User: UserCreated })
+}
+
+
+const createOrder = async function (req, res) {
+    let UserId = req.body.userId
+    let ProductId = req.body.productId
+    if (!UserId) res.send({ Error: "UserId is required" })
+    if (!ProductId) res.send({ Error: "ProductId is required" })
+
+    let Balance = await UserModel.findById(UserId)
+    let Price = await ProductModel.findById(ProductId)
+
+    if (req.isFreeAppUser == false) {
+        if (Balance.balance >= Price.price) {
+            order = req.body
+            order.amount = Price.price
+            order.isFreeAppUser = false
+            let OrderCreated = await OrderModel.create(order)
+            res.send({ data: OrderCreated })
+        }
+        else  res.send({ Error: "Insufficient Balance" })
     }
 
-const createUser= async function (req, res) {
-    
-    let data= req.body
-    let tokenDataInHeaders= req.headers.token
-    //Get all headers from request
-    console.log("Request headers before modificatiom",req.headers)
-    //Get a header from request
-    console.log(req.headers.batch)
-    console.log(req.headers["content-type"])
-    console.log(tokenDataInHeaders)
-    //Set a header in request
-    req.headers['month']='June' //req.headers.month = "June"
-
-    //Set an attribute in request object
-    req.anything = "everything"
-    
-    
-    console.log("Request headers after modificatiom",req.headers)
-    
-    //Set a header in response
-    res.header('year','2022')
-    res.send({msg: "Hi"})
+    if (req.isFreeAppUser == true) {
+        order = req.body
+        order.isFreeAppUser = true;
+        order.amount = 0;
+        let OrderCreated = await OrderModel.create(order)
+        res.send({ data: OrderCreated })
+    }
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+module.exports.createUser = createUser
+module.exports.createProduct = createProduct
+module.exports.createOrder = createOrder
+module.exports.middleware = middleware
+
